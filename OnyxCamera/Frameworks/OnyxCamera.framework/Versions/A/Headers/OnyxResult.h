70cc34e13b5aeb8bfa3df8d116dfe9aef7aee0e7
Matthew Wheatley <mwheatley@diamondfortress.com>
2018-07-02 13:41:46 -0500

    update .podspec added frameworks and image asset
    Former-commit-id: 92599b8fe1e3839f4400615f5768b9c3cd5fbeca

---
+++ OnyxCamera/Frameworks/OnyxCamera.framework/Versions/A/Headers/OnyxResult.h
@@ -0,0 +1,45 @@
+//
+//  OnyxResult.h
+//  OnyxCamera
+//
+//  Created by Matthew Wheatley on 2/3/18.
+//  Copyright © 2018 Diamond Fortress. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "CaptureMetrics.h"
+
+@interface OnyxResult : NSObject
+@property UIImage* grayRawFingerprintImage;
+@property UIImage* rawFingerprintImage;
+@property UIImage* processedFingerprintImage;
+@property UIImage* enhancedFingerprintImage;
+@property UIImage* blackWhiteProcessedFingerprintImage;
+@property NSData* wsqData;
+@property NSData* grayRawWsqData;
+@property NSData* fingerprintTemplate;
+@property CaptureMetrics* captureMetrics;
+
+- (UIImage*) getGrayRawFingerprintImage;
+
+- (UIImage*) getRawFingerprintImage;
+
+- (UIImage*) getProcessedFingerprintImage;
+
+- (UIImage*) getEnhancedFingerprintImage;
+
+- (UIImage*) getBlackWhiteProcessedFingerprintImage;
+
+- (NSData*) getWsqData;
+
+- (NSData*) getGrayRawWsqData;
+
+- (NSData*) getFingerprintTemplate;
+
+- (CaptureMetrics*) getMetrics;
+
+- (NSString*) getFingerprintImageUri:(UIImage*)image;
+- (NSString*) getBase64EncodedString:(NSData*)data;
+
+@end
